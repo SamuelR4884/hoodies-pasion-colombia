7fbc8b9c8259953035f6d1b72bfb95eada583c09
Samuel Ruiz <samuetitc@gmail.com>
2025-12-06 00:40:01 -0500

    Añadida segunda imagen al hoodie Howards Quidditch

--- src/data/products/howardsQuidditch.js
+++ src/data/products/howardsQuidditch.js
@@ -6,7 +6,8 @@ export default {
   title: "Hoodie Howards Quidditch",
   price: 120000,
   images: [
-    "/images/howards-quidditch/howards-quidditch-1.jpg"
+    "/images/howards-quidditch/howards-quidditch-1.jpg",
+    "/images/howards-quidditch/howards-quidditch-2.jpg"
   ],
   description:
     "Hoodie inspirado en Harry Potter. Incluye estampado frontal 'Howards Quidditch', frase trasera 'I solemnly swear that I am up to no good' y logo pequeño de Platform 9 3/4 en la manga."
