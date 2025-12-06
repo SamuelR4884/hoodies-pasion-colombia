0b29bf8102783daa97f71c88fdd46e137123d42e
Samuel Ruiz <samuetitc@gmail.com>
2025-12-05 23:50:57 -0500

    Añadido buso moto

--- src/data/products/index.js
+++ src/data/products/index.js
@@ -1,10 +1,13 @@
 import jake from "./jake";
 import godOfWarGym from "./godOfWarGym";
 import BusoSencillo from "./BusoSencillo";
+import MotoPalabras from "./motoPalabras";
+
 
 
 export default [
   jake,
   godOfWarGym,
-  BusoSencillo
+  BusoSencillo,
+  MotoPalabras
 ];
---
+++ src/data/products/motoPalabras.js
@@ -0,0 +1,14 @@
+import { BASE_PRODUCT } from "./BASE";
+
+export default {
+  ...BASE_PRODUCT,
+  id: "MOTO_PALABRAS",
+  title: "Hoodie moto hecha con palabras",
+  price: 80000,
+  images: [
+    "/images/moto-palabras/moto-palabras-1.jpg",
+    "/images/moto-palabras/moto-palabras-2.jpg"
+  ],
+  description:
+    "Hoodie con diseño de moto formada por palabras como fuerza, velocidad y libertad. Con capota, cordón y bolsillo tipo canguro."
+};
