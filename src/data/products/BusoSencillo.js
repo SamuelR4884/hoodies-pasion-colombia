0d65e0a64918737a6a386a2f4593604549242b03
Samuel Ruiz <samuetitc@gmail.com>
2025-12-05 23:38:18 -0500

    Añadido buso sencillo

---
+++ src/data/products/BusoSencillo.js
@@ -0,0 +1,15 @@
+import { BASE_PRODUCT } from "./BASE";
+
+export default {
+  ...BASE_PRODUCT,
+  id: "BUSO_SENCILLO",
+  title: "Buso sencillo con capota",
+  price: 70000,
+  images: [
+    "/images/buso-sencillo/buso-sencillo-1.jpg",
+    "/images/buso-sencillo/buso-sencillo-2.jpg",
+    "/images/buso-sencillo/buso-sencillo-3.jpg"
+  ],
+  description:
+    "Buso sencillo con capota, cordón y bolsillo tipo canguro. Disponible en varios colores."
+};
--- src/data/products/index.js
+++ src/data/products/index.js
@@ -1,7 +1,10 @@
 import jake from "./jake";
 import godOfWarGym from "./godOfWarGym";
+import BusoSencillo from "./BusoSencillo";
+
 
 export default [
   jake,
-  godOfWarGym
+  godOfWarGym,
+  BusoSencillo
 ];
